3a7de4c87c40810ae5ab352f32fde16a4e0777bf
kushal <kushalmandala29@gmail.com>
2024-03-24 21:27:28 +0530

    error in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+# from model.ipynb import artist_songs
 # import df
 # import simila 
 
@@ -39,6 +40,27 @@ def recommend(song):
         recommended_music_names.append(music.iloc[i[0]].song)
     return recommended_music_names, recommended_music_posters
 
+def get_artist_songs(artist_name):
+    results = sp.search(q='artist:' + artist_name, type='artist')
+
+    # Check if the artist was found
+    if results['artists']['items']:
+    # Get the artist ID
+        artist_id = results['artists']['items'][0]['id']
+    
+    # Get the top tracks for the artist
+        top_tracks = sp.artist_top_tracks(artist_id)
+        track_images=[]
+        for track in top_tracks['tracks']:
+            if track['album']['images']:
+                track_images.append(track['album']['images'][0]['url'])
+            else:
+                track_images.append('https://i.postimg.cc/0QNxYz4V/socia1.png')
+    
+    
+    else:
+        print(f"Artist '{artist_name}' not found.")
+
 st.header('Music Recommender System')
 music=pickle.load(open('df.pkl', 'rb'))
 similarity=pickle.load(open('similar.pkl', 'rb'))
@@ -66,7 +88,24 @@ if st.button('Recommend'):
     with col5:
         st.text(recommended_music_names[4])
         st.image(recommended_music_posters[4])
-        
-        
-        
+if st.button('artist_playlist'):
+    recommended_music_names, recommended_music_posters = get_artist_songs(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_music_names[0])
+        st.image(recommended_music_posters[0])
+    with col2:
+        st.text(recommended_music_names[1])
+        st.image(recommended_music_posters[1])
+    with col3:
+        st.text(recommended_music_names[2])
+        st.image(recommended_music_posters[2])
+    with col4:
+        st.text(recommended_music_names[3])
+        st.image(recommended_music_posters[3])
+    with col5:
+        st.text(recommended_music_names[4])
+        st.image(recommended_music_posters[4])
+      
+
         
